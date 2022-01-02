f9a33c1f6e81961ac5e6bcfb2bb04f4be8cf6c20
Kayota <kayota1992@yandex.ru>
2022-01-02 15:05:20 +0300

    add tests

---
+++ yandex/tests/test.py
@@ -0,0 +1,44 @@
+import unittest
+import page
+import os
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class TestYandex(unittest.TestCase):
+
+
+    def setUp(self):
+        self.options = Options()
+        self.options.add_argument('--start-maximized')
+        self.driver = webdriver.Chrome(options=self.options)
+        self.homepage = page.HomePage(self.driver)
+
+    
+    def test_title_is_matches(self):
+        title = 'Яндекс'
+        self.assertTrue(self.homepage.is_title_matches(title))
+
+    
+    def test_login(self):
+        username = 'kayota1992'[1:]
+        login = os.environ.get('yandex_email')
+        password = os.environ.get('yandex_password')
+        self.homepage.click_btn(self.homepage.locators.get_attr('login_btn'))
+        login_textbox = self.homepage.locators.get_attr('login_textbox')
+        self.homepage.input_text(login_textbox, login)
+        self.homepage.click_btn(self.homepage.locators.get_attr('submit_login_button'))
+        password_textbox = self.homepage.locators.get_attr('password_textbox')
+        self.homepage.input_text(password_textbox, password)
+        self.homepage.click_btn(self.homepage.locators.get_attr('submit_login_button'))
+        self.assertEqual(self.homepage.get_username(), username)
+
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
