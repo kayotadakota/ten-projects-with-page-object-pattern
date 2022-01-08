6f27f48a57287891083831db9b3c8c7bd23f2da0
kayota <kayota1992@yandex.ru>
2022-01-08 19:35:50 +0300

    add login and register page locators

--- src/imdb/locators.py
+++ src/imdb/locators.py
@@ -8,4 +8,18 @@ class HomePageLocators(object):
     twitch = (By.CSS_SELECTOR, 'a[title="Twitch"]')
     twitter = (By.CSS_SELECTOR, 'a[title="Twitter"]')
     youtube = (By.CSS_SELECTOR, 'a[title="YouTube"]')
-    
+    login_btn = (By.CSS_SELECTOR, '.imdb-header__signin-text')   
+
+
+class LoginPageLocators(object):
+
+    create_new_account_btn = (By.CSS_SELECTOR, '.create-account')
+
+
+class RegisterPageLocators(object):
+
+    name_textbox = (By.CSS_SELECTOR, '#ap_customer_name')
+    email_textbox = (By.CSS_SELECTOR, '#ap_email')
+    pass_textbox = (By.CSS_SELECTOR, '#ap_password')
+    pass_check_textbox = (By.CSS_SELECTOR, '#ap_password_check')
+    create_account_btn = (By.CSS_SELECTOR, '#continue')
