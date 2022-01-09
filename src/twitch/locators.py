5a2db9654f0f4c5f33e0fec74c30de84583d19ee
kayota <kayota1992@yandex.ru>
2022-01-09 18:16:13 +0300

    remove search_btn locator

--- src/twitch/locators.py
+++ src/twitch/locators.py
@@ -4,4 +4,3 @@ from selenium.webdriver.common.by import By
 class HomePageLocators:
 
     search_box = (By.CSS_SELECTOR, '#tw-ad3f0aceefe77321b250e2247dbb33e1')
-    search_btn = (By.CSS_SELECTOR, 'button.ScCoreButton-sc-1qn4ixc-0:nth-child(2)')
