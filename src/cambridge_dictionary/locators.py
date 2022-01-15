d187902ad364282e9d327c8337967ce55aef4c76
kayota <kayota1992@yandex.ru>
2022-01-15 17:01:32 +0300

    add err locators

--- src/cambridge_dictionary/locators.py
+++ src/cambridge_dictionary/locators.py
@@ -15,4 +15,5 @@ class RegisterPageLocators:
     confirm_pass_textbox = (By.CSS_SELECTOR, '#register-site-login input[name="passwordRetype"]')
     terms_of_use_checkbox = (By.CSS_SELECTOR, '#register-site-login input[name="data.press.terms.v1.blnAccepted"]')
     submit_btn = (By.CSS_SELECTOR, '#register-site-login input[type="submit"]')
-
+    pass_err_msg = (By.CSS_SELECTOR, '#password-row > div:first-child span')
+    email_err_msg = (By.CSS_SELECTOR, '#register-site-login > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > span:nth-child(2)')
